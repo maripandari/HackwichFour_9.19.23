a21bbf8418a287b5e13e43c5587d432216d0ed5c
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-09-19 13:50:53 -1000

    Completed Parts 1-9

---
+++ HackwichFour_9.19.23/ViewController02.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController02.swift
+//  HackwichFour_9.19.23
+//
+//  Created by Marijoy Jatico on 9/19/23.
+//
+
+import UIKit
+
+class ViewController02: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
